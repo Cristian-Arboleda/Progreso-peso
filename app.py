1095d92d46a9fb25ad1faaead2efe95bfc26f131
Cristian Arboleda <cristianarboledagarzon@gmail.com>
2025-02-12 22:27:25 -0500

    Se termino de crear y el recuadro de inicio de sesion y se le agrego la funcion de dectectar cuando las credenciales son correctas

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # %%
-from dash import Dash, html, dcc, callback, Input, Output, dash_table
+from dash import Dash, html, dcc, callback, Input, Output, State, dash_table
+import json
 
 app = Dash(__name__, external_stylesheets=['assets/style.css'])
 server = app.server
@@ -9,24 +10,17 @@ app.layout = (
         children=[
             html.Div(
                 className='recuadro-inicio-de-sesion',
+                id='recuadro-inicio-de-sesion',
                 children=[
                     html.P(
                         'Iniciar sesion',
-                        style={'text-align': 'center', 'font-size': '25px', 'margin': '0px'}
+                        style={'text-align': 'center', 'font-size': '25px', 'margin': '20px'}
                     ),
-                    html.Div(
-                        children=[
-                            dcc.Input(id='nombre', className='input-inicio-sesion', placeholder='Nombre de usuario', type='text')
-                        ]
-                    ),
-                    html.Div(
-                        children=[
-                            dcc.Input(id='contrasena', className='input-inicio-sesion', placeholder='Password', type='password')
-                        ]
-                    ),
-                    html.Button(
-                        'Enviar',
-                        id='button-inicio-sesion'
+                    dcc.Input(id='nombre_usuario', className='input-inicio-sesion', placeholder='Nombre de usuario', type='text'),
+                    html.P('Nombre de usuario incorecto', id='nombre_incorrecto', className='incorrecto'),
+                    dcc.Input(id='contrasena', className='input-inicio-sesion', placeholder='Password', type='password'),
+                    html.P('Contrasena incorrecta', id='contrasena_incorrecto', className='incorrecto'),
+                    html.Button('Enviar', id='button_enviar',
                     )
                 ]
             )
@@ -34,6 +28,34 @@ app.layout = (
     )
 )
 
+@callback(
+    Output(component_id='nombre_incorrecto', component_property='style'),
+    Output(component_id='contrasena_incorrecto', component_property='style'),
+    Input(component_id='button_enviar', component_property='n_clicks'),
+    State(component_id='nombre_usuario', component_property='value'),
+    State(component_id='contrasena', component_property='value'),
+)
+
+def verificacion_inicio_sesion(n_clicks, nombre_usuario, contrasena):
+    if not n_clicks:
+        return {'display': 'none'}, {'display': 'none'}
+    
+    with open('credenciales.json', 'r') as file:
+        credenciales = json.load(file)
+    
+    # Si el nombre de usuario no existe
+    if nombre_usuario not in credenciales:
+        print('El nombre de usuario no existe')
+        return {'display': 'flex'}, {'display': 'none'}
+    
+    # Si la contrasena es incorrecta
+    if contrasena != credenciales[nombre_usuario]['password']:
+        return {'display': 'none'}, {'display': 'flex'}
+    
+    # si el usuario y la contrasena son correctas
+    return {'display': 'none'}, {'display': 'none'}
+
+
 if __name__ == '__main__':
-    app.run_server(port=8050)
+    app.run_server(port=8050, debug=True)
 # %%
