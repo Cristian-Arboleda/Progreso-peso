119e27945fec088fcd4108f046ea9ecb2373fde1
Cristian Arboleda <cristianarboledagarzon@gmail.com>
2025-02-20 21:40:53 -0500

    update

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-# %%
-from dash import Dash, html, dcc, callback, Input, Output, State
-import json
 
-app = Dash(__name__, use_pages=True, suppress_callback_exceptions=True, external_stylesheets=['assets/login.css'])
-
-server = app.server
-
-
-if __name__ == '__main__':
-    app.run_server(port=8050, debug=True)
-# %%
