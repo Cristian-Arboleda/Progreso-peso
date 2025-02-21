66b649cdf08df51dd0a28296cfa61237d628b5e1
Cristian Arboleda <cristianarboledagarzon@gmail.com>
2025-02-21 16:08:50 -0500

    Se creo el inicio de sesion con sus estilos

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from dash import Dash
 
-app = Dash(__name__,use_pages=True)
+app = Dash(
+    __name__,
+    use_pages=True,
+    external_stylesheets=[
+        'assets/login.css'
+        ]
+)
 
 app.server
 
--- pages/login/login.py
+++ pages/login/login.py
@@ -1,10 +1,15 @@
-from dash import register_page, html, dcc
+from dash import register_page, html, dcc, callback, Input, Output, State
 
 register_page(__name__, path='/')
 
-layout = html.Div(className='recuadro_inicio_sesion', children=[
-    html.P('Iniciar Sesion'),
+layout = html.Div(
+    id='recuadro_inicio_sesion', 
+    children=[
+    html.P('Iniciar Sesion', className='p_iniciar_sesion'),
     dcc.Input(id='nombre_usuario', type='text', placeholder='Nombre de usuario', className='input'),
+    html.P('Nombre usuario incorrecto', id='nombre_usuario_incorrecto', className='p_incorrecto'),
     dcc.Input(id='password', type='password', placeholder='Password', className='input'),
+    html.P('Contrasena incorrecta', id='password_incorrecta', className='p_incorrecto'),
     html.Button('Enviar', id='enviar')
 ])
+
