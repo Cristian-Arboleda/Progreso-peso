2cf1c039449171f5f7f626280acc43171e331d87
Cristian Arboleda <cristianarboledagarzon@gmail.com>
2025-02-22 23:09:07 -0500

    .

--- app.py
+++ app.py
@@ -6,12 +6,9 @@ app = Dash(
     use_pages=True,
 )
 
-app.server
+server = app.server
 
-app.layout =html.Div([
-    
-])
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))  # Render asigna el puerto en PORT
-    app.run_server(host="0.0.0.0", port=port, debug=False)
+    app.run_server(port=8050, debug=True)
