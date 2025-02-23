ad12bc4c3ef84a8e353a0e6fbe9e2c9f196fc275
Cristian Arboleda <cristianarboledagarzon@gmail.com>
2025-02-22 22:34:49 -0500

    update Profile

--- pages/inside/agregar-eliminar.py
+++ pages/inside/agregar-eliminar.py
@@ -3,6 +3,10 @@ from dash import register_page, html
 register_page(__name__, path='/agregar-eliminar')
 
 layout = html.Main([
+    html.Link(
+        rel='stylesheet',
+        href='assets/agregar-eliminar.css',
+    ),
     html.Div(className='agregar', children=[
         html.P('ID:')
     ])
--- pages/login/login.py
+++ pages/login/login.py
@@ -11,14 +11,18 @@ register_page(__name__, path='/')
 layout = html.Div(
     id='recuadro_inicio_sesion', 
     children=[
-    html.P('Iniciar Sesion', className='p_iniciar_sesion'),
-    dcc.Input(id='nombre_usuario', type='text', placeholder='Nombre de usuario', className='input'),
-    html.P('Nombre usuario incorrecto', id='nombre_usuario_incorrecto', className='p_incorrecto'),
-    dcc.Input(id='password', type='password', placeholder='Password', className='input'),
-    html.P('Contrasena incorrecta', id='password_incorrecta', className='p_incorrecto'),
-    html.Button('Enviar', id='enviar'),
-    dcc.Location(id='url'),
-    dcc.Store(id='almacenamiento_datos', storage_type='session')
+        html.Link(
+            rel='stylesheet',
+            href='assets/login.css',
+        ),
+        html.P('Iniciar Sesion', className='p_iniciar_sesion'),
+        dcc.Input(id='nombre_usuario', type='text', placeholder='Nombre de usuario', className='input'),
+        html.P('Nombre usuario incorrecto', id='nombre_usuario_incorrecto', className='p_incorrecto'),
+        dcc.Input(id='password', type='password', placeholder='Password', className='input'),
+        html.P('Contrasena incorrecta', id='password_incorrecta', className='p_incorrecto'),
+        html.Button('Enviar', id='enviar'),
+        dcc.Location(id='url'),
+        dcc.Store(id='almacenamiento_datos', storage_type='session')
 ])
 
 @callback(
