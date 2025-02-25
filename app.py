fb4ff29ddd3013cd1eab48a4f1bd14f788a261a7
Cristian Arboleda <cristianarboledagarzon@gmail.com>
2025-02-25 14:57:36 -0500

    Se corrigio un error en el que hacia que el menu de paginas se mostrara en el login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, page_container, callback, Input, Output, State
+from dash import Dash, html, dcc, page_container, callback, Input, Output, State, no_update
 import os
 
 
@@ -15,21 +15,28 @@ app.layout = html.Div([
         rel='stylesheet',
         href='assets/menu_de_paginas.css'
     ),
-    html.Div(id = 'menu_de_paginas_contenedor'),
+    dcc.Store('almacenamiento_datos', storage_type='session'),
+    dcc.Location(id='url'),
+    
+    #------------------------------------------------------
+    html.Div(id = 'menu_de_paginas_contenedor', style={'display': 'none'}),
     page_container,
-    dcc.Store('almacenamiento_datos', storage_type='session')
 ])
 
 
 # mostrar encabezado de las paginas despues de iniciar sesion
 @callback(
     Output(component_id='menu_de_paginas_contenedor', component_property='children'),
+    Output(component_id='menu_de_paginas_contenedor', component_property='style'),
     Input(component_id='almacenamiento_datos', component_property='data'),
+    Input(component_id='url', component_property='pathname'),
 )
 
-def lista_paginas(data):
-    if not data:
-        return ''
+def lista_paginas(data, url):
+    
+    print(f'url actual: {url}')
+    if url != '/':
+        return no_update, no_update
     
     # Obtener la lista de las paginas dentro de la carpeta pages e inside
     carpeta = 'pages/inside'
@@ -38,11 +45,12 @@ def lista_paginas(data):
     # Quitar extension .py de los arhivos
     
     lista_paginas = [os.path.splitext(archivo)[0] for archivo in lista_paginas]
-    
+    print(f'lista de paginas obtenidas: {lista_paginas}')
     # Crear los links de las paginas
     
     lista_paginas = [dcc.Link(archivo, href=archivo, className='pagina_de_menu') for archivo in lista_paginas]
-    return lista_paginas
+    
+    return lista_paginas, {'display': 'flex'}
 
 
 if __name__ == "__main__":
