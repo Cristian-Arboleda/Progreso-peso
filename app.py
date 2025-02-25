f4f5d16b8a95f001fa575c222e29f683cf7c7b32
Cristian Arboleda <cristianarboledagarzon@gmail.com>
2025-02-24 23:36:36 -0500

    Se creo el menu de paginas el cual aparece despues de iniciar sesion

--- app.py
+++ app.py
@@ -1,13 +1,48 @@
-from dash import Dash, html
+from dash import Dash, html, dcc, page_container, callback, Input, Output, State
 import os
+
 
 app = Dash(
     __name__,
-    use_pages=True,
+    use_pages=True, # Permite que se puedan registrar otras paginas
+    suppress_callback_exceptions=True, # Esto evitará que Dash arroje errores cuando el callback se defina antes de que el componente aparezca en el layout.
 )
 
 server = app.server
 
+app.layout = html.Div([
+    html.Link(
+        rel='stylesheet',
+        href='assets/menu_de_paginas.css'
+    ),
+    html.Div(id = 'menu_de_paginas_contenedor'),
+    page_container,
+    dcc.Store('almacenamiento_datos', storage_type='session')
+])
+
+
+# mostrar encabezado de las paginas despues de iniciar sesion
+@callback(
+    Output(component_id='menu_de_paginas_contenedor', component_property='children'),
+    Input(component_id='almacenamiento_datos', component_property='data'),
+)
+
+def lista_paginas(data):
+    if not data:
+        return ''
+    
+    # Obtener la lista de las paginas dentro de la carpeta pages e inside
+    carpeta = 'pages/inside'
+    lista_paginas = [archivo for archivo in os.listdir(carpeta) if archivo.endswith('.py')]
+    
+    # Quitar extension .py de los arhivos
+    
+    lista_paginas = [os.path.splitext(archivo)[0] for archivo in lista_paginas]
+    
+    # Crear los links de las paginas
+    
+    lista_paginas = [dcc.Link(archivo, href=archivo, className='pagina_de_menu') for archivo in lista_paginas]
+    return lista_paginas
 
 
 if __name__ == "__main__":
--- pages/inside/agregar-eliminar.py
+++ pages/inside/agregar-eliminar.py
@@ -1,13 +1,114 @@
-from dash import register_page, html
+from dash import register_page, html, dcc, callback, Input, Output, State, dash_table
+import os
+import json
+from datetime import date
+
 
 register_page(__name__, path='/agregar-eliminar')
 
-layout = html.Main([
+layout = html.Div([
     html.Link(
         rel='stylesheet',
         href='assets/agregar-eliminar.css',
     ),
-    html.Div(className='agregar', children=[
-        html.P('ID:')
+    dcc.Store(id='almacenamiento_datos', storage_type='session'),
+    
+    # ------------------------------------------------------
+    
+    html.Div(id='titulo_nombre_usuario'),
+    html.Main([
+        html.H1('Agregar'),
+        html.Div(id='agregar', children=[
+            html.Label('ID:', htmlFor='id_agregar'),
+            dcc.Input(id='id_agregar', type='number',),
+            html.Label('Fecha:', htmlFor='fecha_agregar'),
+            dcc.Input(id='fecha_agregar', type='text'),
+            html.Label('Ciclo:', htmlFor='ciclo_agregar'),
+            dcc.Dropdown(
+                id='ciclo_agregar',
+                options=[
+                    {'label': 'Diurno', 'value': 'diurno'},
+                    {'label': 'Nocturno', 'value': 'nocturno'}
+                ],
+                value='diurno',
+                clearable=False,
+                style={'width': '150px', 'font-size': '20px'}
+            ),
+            html.Label('Peso:'),
+            dcc.Input(id='peso_agregar', type='number'),
+            html.Button('Agregar', id='agregar_button')
+        ]),
+        html.Hr(),
+        html.H1('Eliminar'),
+        html.Div(id='eliminar', children=[
+            
+        ]),
+        dash_table.DataTable(
+            id='database_table',
+            columns=[
+                {"name": "ID", "id": "id"},
+                {"name": "Nombre", "id": "nombre"},
+                {"name": "Edad", "id": "edad"}
+            ],
+            data=[
+                {"id": 1, "nombre": "Carlos", "edad": 25},
+            ]
+        )
     ])
 ])
+
+
+# obtener el path de database.json
+
+current_dir = os.path.dirname(__file__)  
+database_path = os.path.join(current_dir, "database.json")
+
+
+# nombre del usuario 
+
+@callback(
+    Output(component_id='titulo_nombre_usuario', component_property='children'),
+    Input(component_id='almacenamiento_datos', component_property='data'),
+)
+
+def update_nombre_usuario(almacenamiento_datos):
+    if almacenamiento_datos is None:
+        print('No se encuentra el nombre del usuario que inicio sesion')
+        return 'Nada'
+    print('Estableciendo el nombre de usuario')
+    nombre_usuario = almacenamiento_datos['sesion_iniciada_por']
+    return nombre_usuario
+
+
+# actualizar valores de input
+
+@callback(
+    Output(component_id='id_agregar', component_property='value' ),
+    Output(component_id='fecha_agregar', component_property='value'),
+    Input(component_id='almacenamiento_datos',component_property='data'),
+)
+
+def update_valores_inputs(almacenamiento_datos):
+    print('Actualizando los valores de los inputs de agregar')
+    
+    # Obtener el nombre de quien haya iniciado sesion
+    nombre_usuario = almacenamiento_datos['sesion_iniciada_por']
+    
+    # Obtener la base de datos
+    
+    with open(database_path, 'r') as file:
+        database = json.load(file)
+    
+    
+    # Obtener siguiente ID
+    siguiente_id = int(list(database[nombre_usuario].keys())[-1]) + 1 
+    
+    # Obtener la fecha actual
+    
+    fecha_actual = date.today()
+    
+    return siguiente_id, fecha_actual
+
+
+
+
